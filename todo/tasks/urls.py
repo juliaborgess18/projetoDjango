cd67f22b70d5b6063fd1ec9a454cdb736687cbb8
Júlia Borges <62912240+juliaborgess18@users.noreply.github.com>
2023-10-14 09:42:14 -0300

    Delete todo/tasks directory

--- todo/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("helloworld/", views.helloWorld),
-    path("", views.tasksList, name='task-list'),
-    path("yourname/<str:name>", views.yourName, name='yourname'),
-
-]
