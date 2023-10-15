25581eff82f4b0fc7578c08c3b47f6086e417652
Júlia Borges <juliaborgess18@gmail.com>
2023-10-15 11:55:30 -0300

    Adicionando tarefas no banco de dados por forms

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # path("", views.helloWorld, name='helloWorld'),
     path("", views.listaTarefas, name='listaTarefas'),
-    path("task/<int:id>", views.taskView, name='listaTarefa'),
+    path("task/<int:id>", views.taskViewSpecific, name='listaTarefaEspecífica'),
+    path("newtask/", views.newTask, name="newTask"),
     path("yourName/<str:name>", views.retornaNome, name='retornaNome'),
 ]
