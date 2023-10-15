f6bf0cc46568c072e346f1a41a1c5c73e77cc670
Júlia Borges <juliaborgess18@gmail.com>
2023-10-15 13:34:39 -0300

    Configurando ação de editar dados

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.listaTarefas, name='listaTarefas'),
     path("task/<int:id>", views.taskViewSpecific, name='listaTarefaEspecífica'),
     path("newtask/", views.newTask, name="newTask"),
+    path("edittask/<int:id>", views.editTask, name="editTask"),
     path("yourName/<str:name>", views.retornaNome, name='retornaNome'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -24,6 +24,19 @@ def newTask(request):
     form = TaskForm()
     return render(request, 'tasks/addtask.html', {'form': form})
 
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+
+    if(request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+
+        if (form.is_valid()):
+            task.save()
+            return redirect('/')
+
+    return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
 def helloWorld(request):
     return HttpResponse('hello World')
 
