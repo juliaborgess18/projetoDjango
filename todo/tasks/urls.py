400a05d9a9e3254ff7f4fc14cd9155648df3f2b0
Júlia Borges <juliaborgess18@gmail.com>
2023-10-15 10:32:32 -0300

    Listando através dos dados armazenados

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # path("", views.helloWorld, name='helloWorld'),
     path("", views.listaTarefas, name='listaTarefas'),
+    path("task/<int:id>", views.taskView, name='listaTarefa'),
     path("yourName/<str:name>", views.retornaNome, name='retornaNome'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,12 +1,18 @@
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
+def listaTarefas(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task':task})
+
 def helloWorld(request):
     return HttpResponse('hello World')
-
-def listaTarefas(request):
-    return render(request, 'tasks/list.html')
 
 def retornaNome(request, name):
     return render(request, 'tasks/yourname.html', {'name': name})
