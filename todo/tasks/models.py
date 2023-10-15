01f9127b82a2ece2ad21920385723ccb591beaa7
Júlia Borges <juliaborgess18@gmail.com>
2023-10-15 10:12:29 -0300

    Configurando área administrativa

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+
+    STATUS = (
+        (1, 'Doing'),
+        (2, 'Done'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.CharField(
+        max_length=1, 
+        choices=STATUS,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
