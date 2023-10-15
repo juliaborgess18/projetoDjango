73ad85ee595a1f0ac3e843e1c63c07c7215f222a
Júlia Borges <juliaborgess18@gmail.com>
2023-10-15 08:44:40 -0300

    Estilizando página de lista de tarefas

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.helloWorld, name='helloWorld'),
-    path("listaTarefas/", views.listaTarefas, name='listaTarefas'),
+    # path("", views.helloWorld, name='helloWorld'),
+    path("", views.listaTarefas, name='listaTarefas'),
     path("yourName/<str:name>", views.retornaNome, name='retornaNome'),
 ]
