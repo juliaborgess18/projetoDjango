fac32158ec382e91d6f7a9697023fb3c56cfefb1
Julia <juliaborgess18@gmail.com>
2023-10-16 15:15:23 -0300

    Adicionando paginação com classe Paginator

--- projetoToDo/todo/tasks/views.py
+++ projetoToDo/todo/tasks/views.py
@@ -3,10 +3,18 @@ from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 def listaTarefas(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 3)
+
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
+
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 def taskViewSpecific(request, id):
